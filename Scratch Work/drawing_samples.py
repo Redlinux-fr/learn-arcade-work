45b806dd9afab2aa7f76769461c54e5bbad0a895
Redlinux-fr <redlinux.56@gmail.com>
2023-04-11 00:14:08 +0200

    chapitre 5, para 5.4

---
+++ Scratch Work/drawing_samples.py
@@ -0,0 +1,12 @@
+"""
+La norme est de mettre des triples guillemets en debut de programme
+pour expliquer ce que le code va faire .
+"""
+
+# apres on utilise le diese pour faire des commentaire ponctuel
+
+import arcade
+
+print("chapitre 5")
+
+arcade.open_windows(600, 600, "Fenetre Arcade")
