7a44c85164cbf1042ff02d75a3ac2071193e5aa2
Redlinux-fr <redlinux.56@gmail.com>
2023-04-12 01:13:48 +0200

    chapitre 5.6

--- Scratch Work/drawing_samples.py
+++ Scratch Work/drawing_samples.py
@@ -9,4 +9,21 @@ import arcade
 
 print("chapitre 5")
 
-arcade.open_windows(600, 600, "Fenetre Arcade")
+# definie les dimensions de la fenetre
+arcade.open_window(600, 600, "Fenetre Arcade")
+
+
+
+
+# on definie la couleur d'arriere plan ( les trois code couleur RVB )
+arcade.set_background_color((24, 230, 40))
+
+# dit a python.arcade qu'on commencer a dessiner / afficher quelque chose
+arcade.start_render()
+
+# on indique le code pour dessiner ici
+
+#  dit a python.arcade qu'on a finit de dessiner
+arcade.finish_render()
+
+arcade.run()
