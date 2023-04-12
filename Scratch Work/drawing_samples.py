cf2b40490ed592f2f654a2e64ca400ca3e0d79f1
Redlinux-fr <redlinux.56@gmail.com>
2023-04-13 00:38:01 +0200

    dessiner des formes

--- Scratch Work/drawing_samples.py
+++ Scratch Work/drawing_samples.py
@@ -16,12 +16,16 @@ arcade.open_window(600, 600, "Fenetre Arcade")
 
 
 # on definie la couleur d'arriere plan ( les trois code couleur RVB )
-arcade.set_background_color((24, 230, 40))
+arcade.set_background_color(arcade.csscolor.WHITE)
 
 # dit a python.arcade qu'on commencer a dessiner / afficher quelque chose
 arcade.start_render()
 
 # on indique le code pour dessiner ici
+arcade.draw_lrtb_rectangle_filled(0, 100, 100, 0, arcade.csscolor.BLACK)
+arcade.draw_lrtb_rectangle_filled(101, 200, 200, 101, arcade.csscolor.BLACK)
+
+
 
 #  dit a python.arcade qu'on a finit de dessiner
 arcade.finish_render()
